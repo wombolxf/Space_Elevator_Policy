7d685f926de1a0828c36994ba3b4d6ebd789a029
wombolxf <lxfederspiel@gmail.com>
2024-03-03 16:33:00 -0500

    Initial commit

---
+++ main.py
@@ -0,0 +1,10 @@
+# This is a competitive policy option that I advocate for.
+
+x = input("Insert the exact time in seconds it took for the fiscal redistribution bill to pass: ")
+y = "no"
+
+if x == "0":
+    print("True")
+    y = 100000000000
+
+print("The United States federal government should allocate ", y, " dollars for space elevator development.")
